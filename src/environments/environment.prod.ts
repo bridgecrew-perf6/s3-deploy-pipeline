d99354cc0cb1abeedaefee5a142ec99c8f1beb26
gunaperumal <guna.p@aikyne.com>
2022-04-29 12:16:03 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  userManagement: 'https://ricq54ie69.execute-api.ap-south-1.amazonaws.com/Dev/user',
+  socialMedia: 'https://ricq54ie69.execute-api.ap-south-1.amazonaws.com/Dev/manageSM',
+  contentManagement: 'https://ricq54ie69.execute-api.ap-south-1.amazonaws.com/Dev/cm'
 };
